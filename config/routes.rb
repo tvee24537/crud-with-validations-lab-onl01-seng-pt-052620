a83e69b7ab8257e6fde33635617d13ff4699735e
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-10-12 20:41:11 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,32 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'songs/index'
+
+  get 'songs/edit'
+
+  get 'songs/update'
+
+  get 'songs/new'
+
+  get 'songs/create'
+
+  get 'songs/delete'
+
+  get 'songs/show'
+
+  get 'songs/index'
+
+  get 'songs/edit'
+
+  get 'songs/update'
+
+  get 'songs/new'
+
+  get 'songs/create'
+
+  get 'songs/delete'
+
+  get 'songs/show'
+
+  resources :songs
 end
